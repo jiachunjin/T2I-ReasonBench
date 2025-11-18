c89e24828557b359bc6f030918697987ab76c1b8
Jiachun Jin <jiachun22.jin@gmail.com>
2025-11-18 11:29:53 +0800

    s

--- eval.sh
+++ eval.sh
@@ -1,9 +1,9 @@
-NAME="qimage_flow_grpo"
-IMAGE_PATH="qimage_flow_grpo"
+NAME="unicot"
+IMAGE_PATH="unicot"
 WORKERS=16
 
 python evaluation/Qwen2.5-VL/eval_entity.py \
-  --image_folder /data/phd/jinjiachun/codebase/samples_qwenimage/${IMAGE_PATH}/entity_reasoning \
+  --image_folder /data/phd/jinjiachun/codebase/UniCoT/results/entity_reasoning \
   --output_path csv_result/entity \
   --prompt_json prompts/entity_reasoning.json \
   --qs_json deepseek_evaluation_qs/evaluation_entity.json \
@@ -11,7 +11,7 @@ python evaluation/Qwen2.5-VL/eval_entity.py \
   --num_workers ${WORKERS}
 
 python evaluation/Qwen2.5-VL/eval_idiom.py \
-  --image_folder /data/phd/jinjiachun/codebase/samples_qwenimage/${IMAGE_PATH}/idiom_interpretation \
+  --image_folder /data/phd/jinjiachun/codebase/UniCoT/results/idiom_interpretation \
   --output_path csv_result/idiom \
   --prompt_json prompts/idiom_interpretation.json \
   --qs_json deepseek_evaluation_qs/evaluation_idiom.json \
@@ -19,7 +19,7 @@ python evaluation/Qwen2.5-VL/eval_idiom.py \
   --num_workers ${WORKERS}
 
 python evaluation/Qwen2.5-VL/eval_scientific.py \
-  --image_folder /data/phd/jinjiachun/codebase/samples_qwenimage/${IMAGE_PATH}/scientific_reasoning \
+  --image_folder /data/phd/jinjiachun/codebase/UniCoT/results/scientific_reasoning \
   --output_path csv_result/scientific \
   --prompt_json prompts/scientific_reasoning.json \
   --qs_json deepseek_evaluation_qs/evaluation_scientific.json \
@@ -27,7 +27,7 @@ python evaluation/Qwen2.5-VL/eval_scientific.py \
   --num_workers ${WORKERS}
 
 python evaluation/Qwen2.5-VL/eval_textual_image.py \
-  --image_folder /data/phd/jinjiachun/codebase/samples_qwenimage/${IMAGE_PATH}/textual_image_design \
+  --image_folder /data/phd/jinjiachun/codebase/UniCoT/results/textual_image_design \
   --output_path csv_result/textual_image \
   --prompt_json prompts/textual_image_design.json \
   --qs_json deepseek_evaluation_qs/evaluation_textual_image.json \
